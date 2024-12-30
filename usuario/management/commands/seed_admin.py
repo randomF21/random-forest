3589db7deff6844d88deaf514200989035223608
Oftal210 <redsunestudio@gmail.com>
2024-12-30 11:12:24 -0500

    agrego archivos para las semillas, insercion de los roles y 3 tipos de usuario con un solo comando

---
+++ usuario/management/commands/seed_admin.py
@@ -0,0 +1,65 @@
+from django.core.management.base import BaseCommand
+from usuario.models import CustomUser, Rol
+
+class Command(BaseCommand):
+    help = 'Seed para crear los usuarios de superadmin y admin'
+
+    def handle(self, *args, **kwargs):
+        
+        roles = {
+            'super': Rol.objects.get(id=1),
+            'admin': Rol.objects.get(id=2),
+            'user': Rol.objects.get(id=3)
+        }
+        
+        usuarios = [
+            {
+                'password': '1234',
+                'email': 'super@gmail.com',
+                'nombre':'super',
+                'apellido':'admin',
+                'tipo_documento':'Registro Civil',
+                'num_documento':'1234567890',
+                'telefono':'3214105263',
+                'nacimiento':'2001-01-01',
+                'rol': roles['super']
+            },
+            {
+                'password': '1234',
+                'email': 'admin24@gmail.com', # quitar el 24
+                'nombre':'admin',
+                'apellido':'normal',
+                'tipo_documento':'Registro Civil',
+                'num_documento':'1234567890',
+                'telefono':'3214105263',
+                'nacimiento':'2001-01-01',
+                'rol': roles['admin']
+            },
+            {
+                'password': '1234',
+                'email': 'user@gmail.com',
+                'nombre':'usuario',
+                'apellido':'normal',
+                'tipo_documento':'Registro Civil',
+                'num_documento':'1234567890',
+                'telefono':'3214105263',
+                'nacimiento':'2001-01-01',
+                'rol': roles['user']
+            }
+        ]
+
+        for usuario in usuarios: 
+            user = CustomUser( 
+                email=usuario['email'], 
+                nombre=usuario['nombre'], 
+                apellido=usuario['apellido'], 
+                tipo_documento=usuario['tipo_documento'], 
+                num_documento=usuario['num_documento'], 
+                telefono=usuario['telefono'], 
+                nacimiento=usuario['nacimiento'], 
+                rol=usuario['rol']
+            ) 
+            user.set_password(usuario['password']) 
+            user.save()
+        
+        self.stdout.write(self.style.SUCCESS('Se inserto el superadmin, el admin y un usuario :D'))
--- usuario/management/commands/seed_roles.py
+++ usuario/management/commands/seed_roles.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from usuario.models import Rol
 
 class Command(BaseCommand):
-    help = 'Seed para la base de datos con los roles iniciales'
+    help = 'Seed para crear los roles del sistema'
 
     def handle(self, *args, **kwargs):
         roles = [
@@ -14,4 +14,4 @@ class Command(BaseCommand):
         for role in roles:
             Rol.objects.get_or_create(nombre=role)
         
-        self.stdout.write(self.style.SUCCESS('Se insertaron correctamente'))
+        self.stdout.write(self.style.SUCCESS('Se insertaron los roles :D'))
